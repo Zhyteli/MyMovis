0ca20504179ef1c7ef2f34f467f8ae74a4d9378b
Asus <kirill.zhikhor15@gmail.com>
2022-07-14 23:46:22 +0300

    Application logic and getting JSON

--- app/src/main/java/com/example/mymovis/presentation/MainActivity.java
+++ app/src/main/java/com/example/mymovis/presentation/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.mymovis.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.mymovis.R;
+import com.example.mymovis.data.Movie;
+import com.example.mymovis.utils.JSONUtils;
+import com.example.mymovis.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for (Movie movie: movies){
+            builder.append(movie.getTitle()).append("\n");
+        }
+        Log.d("MyResult", builder.toString());
     }
 }
